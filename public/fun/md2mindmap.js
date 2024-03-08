9edf413a6dadbfe2976c866bbf393c8b45df963c
weekend-project-space <sunxzg@gmail.com>
2024-03-08 16:08:36 +0800

    -=

--- public/fun/md2mindmap.js
+++ public/fun/md2mindmap.js
@@ -1,6 +1,6 @@
 async () => {
   return {
-    content: `<iframe src="https://plugins.1e1.fun/md2mindmap.html"  width="700px" height="500px" frameborder="0" scrolling="no"> </iframe>`,
+    content: `<iframe src="https://plugins.1e1.fun/md2mindmap.html"  width="100%" height="400px" frameborder="0" scrolling="no"> </iframe>`,
     lazyfun: `(el, args) => {
       var iframe = el.getElementsByTagName('iframe')[0].contentWindow;
       iframe.postMessage(args.content.replaceAll('\\\\\\\\n','\\n').replaceAll('\\\\n','\\n'), '*');
